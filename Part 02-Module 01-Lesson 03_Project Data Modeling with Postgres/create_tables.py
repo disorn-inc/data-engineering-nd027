807ef298eb3bf24c8e27d8f06978f80985c8cb4a
dhrey <d.adeleke@semicolon.africa>
2021-02-08 00:22:35 +0100

    Updating create_table.py

--- Part 02-Module 01-Lesson 03_Project Data Modeling with Postgres/create_tables.py
+++ Part 02-Module 01-Lesson 03_Project Data Modeling with Postgres/create_tables.py
@@ -7,11 +7,14 @@ def create_db():
     -   Connect to sparkifydb
     """
 
+    # Connect to default db
     try:
         conn = psycopg2.connect("host=localhost user=postgres password=uncle1dee")
     except psycopg2.Error as e:
-        print("Error: Could not make connection to the Postgres database")
+        print("Error: Could not make connection to the Postgres database")\
 
+
+    # Create the cursor
     try:
         cur = conn.cursor()
     except psycopg2.Error as e:
@@ -19,6 +22,33 @@ def create_db():
         print(e)
 
     conn.set_session(autocommit=True)
+
+    # Create sparkify db UTF8 encoding
+    try:
+        cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+        cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE  template0")
+    except psycopg2.Error as e:
+        print("Error: Could not create the database")
+        print(e)
+
+    # close connection to default database
+    conn.close()
+
+    # Connect to sparkifydb
+    try:
+        conn = psycopg2.connect("host=localhost dbname=sparkifydb user=postgres password=uncle1dee")
+    except psycopg2.Error as e:
+        print("Error: Could not make connection to the sparkifydb database")
+        print(e)
+
+    # Create the cursor
+    try:
+        cur = conn.cursor()
+    except psycopg2.Error as e:
+        print("Error: Could not get cursor to the database")
+        print(e)
+
+    return cur, conn
 
 
 create_db()
