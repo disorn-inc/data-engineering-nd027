12b8d45a240f3536a05f753cdd8ecd114635998c
dhrey <d.adeleke@semicolon.africa>
2021-02-09 04:01:32 +0100

    creating etl.py file

--- Part 02-Module 01-Lesson 03_Project Data Modeling with Postgres/etl.py
+++ Part 02-Module 01-Lesson 03_Project Data Modeling with Postgres/etl.py
@@ -59,3 +59,23 @@ def process_log_file(cur, filepath):
         cur.execute(insert_songplay_table, songplay_data)
 
 
+def process_data(cur, conn, filepath, func):
+    """Iterate over all files matching extension and process those files."""
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root, '*json'))
+        for f in files:
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print("{} files found in {}".format(num_files, filepath))
+
+    # Iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
+
